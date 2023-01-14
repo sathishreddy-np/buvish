d7f5131629de2ede74c1c02a8a852c7ff2bd7af8
sathishreddy.np <sathishreddy.np@gmail.com>
2023-01-14 14:19:48 +0530

    machine beverages

---
+++ app/Http/Controllers/BeverageMachineController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreBeverageMachineRequest;
+use App\Http\Requests\UpdateBeverageMachineRequest;
+use App\Models\BeverageMachine;
+use App\Models\Machine;
+use Illuminate\Http\Request;
+
+class BeverageMachineController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Machine $machine)
+    {
+        return $machine->beverages()->get();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreBeverageMachineRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreBeverageMachineRequest $request,Machine $machine)
+    {
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\BeverageMachine  $beverageMachine
+     * @return \Illuminate\Http\Response
+     */
+    public function show(BeverageMachine $beverageMachine)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\BeverageMachine  $beverageMachine
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(BeverageMachine $beverageMachine)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateBeverageMachineRequest  $request
+     * @param  \App\Models\BeverageMachine  $beverageMachine
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateBeverageMachineRequest $request, BeverageMachine $beverageMachine, Machine $machine)
+    {
+        $beverage_ids = explode(',',$request->beverage_ids);
+        return $machine->beverages()->sync($beverage_ids);
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\BeverageMachine  $beverageMachine
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(BeverageMachine $beverageMachine, Machine $machine, Request $request)
+    {
+
+    }
+}
---
+++ database/migrations/2023_01_14_083442_create_beverage_machine_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('beverage_machine', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('beverage_id');
+            $table->unsignedBigInteger('machine_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('beverage_machine');
+    }
+};
