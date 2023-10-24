083278a122951c4891c174346f8f2bc5048d3ae6
sathishreddy-np <sathishreddy.np@gmail.com>
2023-10-24 20:30:35 +0530

    no one can edit or delete permissions

--- app/Filament/Resources/RoleResource/RelationManagers/UsersRelationManager.php
+++ app/Filament/Resources/RoleResource/RelationManagers/UsersRelationManager.php
@@ -6,8 +6,11 @@ use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
+use Filament\Tables\Enums\FiltersLayout;
+use Filament\Tables\Filters\TernaryFilter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class UsersRelationManager extends RelationManager
@@ -32,10 +35,64 @@ class UsersRelationManager extends RelationManager
                 Tables\Columns\TextColumn::make('name')
                 ->searchable()
                 ->sortable(),
+                Tables\Columns\TextColumn::make('email')
+                ->sortable()
+                ->searchable(),
+            Tables\Columns\TextColumn::make('roles.name')
+                ->badge()
+                ->sortable()
+                ->searchable()
+                ->getStateUsing(function (Model $record) {
+                    $roles = $record->roles->toArray();
+                    $role_names = array_map(function ($role) {
+                        return $role['name'];
+                    }, $roles);
+                    return $role_names;
+                }),
+            Tables\Columns\IconColumn::make('is_verified')
+                ->sortable()
+                ->icon(fn (string $state): string => match ($state) {
+                    '0' => 'heroicon-o-x-circle',
+                    '1' => 'heroicon-o-check-badge',
+                    default => 'heroicon-o-check-circle',
+                })
+                ->color(fn (string $state): string => match ($state) {
+                    '0' => 'danger',
+                    '1' => 'success',
+                    default => 'gray',
+                }),
+            Tables\Columns\ToggleColumn::make('is_active')
+                ->sortable(),
+            Tables\Columns\TextColumn::make('email_verified_at')
+                ->dateTime()
+                ->sortable()
+                ->toggleable(isToggledHiddenByDefault: true),
+            Tables\Columns\TextColumn::make('created_at')
+                ->dateTime()
+                ->sortable()
+                ->toggleable(isToggledHiddenByDefault: true),
+            Tables\Columns\TextColumn::make('updated_at')
+                ->dateTime()
+                ->sortable()
+                ->toggleable(isToggledHiddenByDefault: true),
+            Tables\Columns\TextColumn::make('deleted_at')
+                ->dateTime()
+                ->sortable()
+                ->toggleable(isToggledHiddenByDefault: true),
+
+
             ])
             ->filters([
-                //
-            ])
+                Tables\Filters\TrashedFilter::make(),
+                TernaryFilter::make('is_verified')
+                    ->label('User verified')
+                    ->placeholder('Select status')
+                    ->nullable(),
+                TernaryFilter::make('is_active')
+                    ->label('Is active')
+                    ->placeholder('Select status')
+                    ->nullable(),
+            ], layout: FiltersLayout::AboveContentCollapsible)
             ->headerActions([
                 Tables\Actions\AttachAction::make(),
             ])
